3c0beb0fd97d59aabc12aecffea2ab1d151c2751
Shijia003 <68273740+Shijia003@users.noreply.github.com>
2025-07-01 00:04:26 +0800

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -26,12 +26,14 @@ def connect_with_middleware(contract_json):
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	w3 = 0
-
+	w3 = Web3(HTTPProvider("https://bsc-testnet-rpc.publicnode.com"))
+	assert w3.is_connected(), f"Failed to connect to provider at {url}"
 	# The second section requires you to inject middleware into your w3 object and
 	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
 	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
-	contract = 0
+	w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
+	# w3.middleware_onion.inject(web3.middleware.GasPriceStrategyMiddleware, layer=0)
+	contract = w3.eth.contract(address=address, abi=abi)
 
 	return w3, contract
 
