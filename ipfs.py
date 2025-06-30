2e4a776e41663fdb19c1f5cd3a98fc2e82cdab33
Shijia003 <68273740+Shijia003@users.noreply.github.com>
2025-07-01 00:34:32 +0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -1,11 +1,27 @@
 import requests
 import json
 
+PINATA_BASE_URL = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
+PINATA_GATEWAY = "https://gateway.pinata.cloud/ipfs/"
+PINATA_API_KEY = "643065627ab15d753e71"
+PINATA_SECRET_API_KEY = "01714bb5dacce226d92ab7d60eb474dc535b4a9210d1e6d8808f116da847de01"
+
 def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 	#YOUR CODE HERE
-	# cid = data["cid"]
-	print(data)
+	headers = {
+		"Content-Type": "application/json",
+		"pinata_api_key": PINATA_API_KEY,
+		"pinata_secret_api_key": PINATA_SECRET_API_KEY,
+  }
+
+  response = requests.post(PINATA_BASE_URL, data=json.dumps({"pinataContent": data}), headers=headers)
+
+  if response.status_code != 200:
+      raise Exception(f"Failed to pin JSON to IPFS: {response.text}")
+
+  cid = response.json()["IpfsHash"]
+
 	return cid
 
 def get_from_ipfs(cid,content_type="json"):
