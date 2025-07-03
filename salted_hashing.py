3ec2943996479d690aafd006b13b87ae935c4225
Shijia003 <68273740+Shijia003@users.noreply.github.com>
2025-07-03 17:07:48 +0800

    Update salted_hashing.py

--- salted_hashing.py
+++ salted_hashing.py
@@ -4,6 +4,8 @@ import random
 
 def my_hash(m):
     #Generate random nonce
+    if not isinstance(m, str):
+        raise TypeError("Input must be a string")
     nonce = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
     #Generate hex digest
     combined = nonce + m
