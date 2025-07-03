de1efb46412e121e19730aa4cd9eda280c53d18a
Shijia003 <68273740+Shijia003@users.noreply.github.com>
2025-07-03 16:53:32 +0800

    Update salted_hashing.py

--- salted_hashing.py
+++ salted_hashing.py
@@ -1,11 +1,11 @@
 import hashlib
 import os
 
-def myhash(m):
+def my_hash(m):
     #Generate random nonce
     nonce = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
     #Generate hex digest
-    combined = s + nonce
+    combined = m + nonce
     hash_object = hashlib.sha256(combined.encode())
     h_hex = hash_object.hexdigest() 
     return nonce, h_hex
