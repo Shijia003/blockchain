3da9ee262586e1324f15a6f945e4e991b8e0b20d
Shijia003 <68273740+Shijia003@users.noreply.github.com>
2025-07-03 23:15:38 +0800

    Update reading_the_chain.py

--- reading_the_chain.py
+++ reading_the_chain.py
@@ -64,13 +64,15 @@ def is_ordered_block(w3, block_num):
 
 	for tx in txs:
 	    	# type 2
-		if tx.gasPrice is None and tx.type == '2':
-	        	priority_fee = min(tx.maxPriorityFeePerGas,tx.maxFeePerGas - base_fee)
-		elif tx.gasPrice is not None and tx.type == '2':  
-	        	priority_fee = tx.maxPriorityFeePerGas
-		else:
-	        	priority_fee = tx.gasPrice - base_fee
-			# raise ValueError(f"Unsupported transaction type: {tx.type}")
+		# if tx.gasPrice is None and tx.type == '2':
+	 #        	priority_fee = min(tx.maxPriorityFeePerGas,tx.maxFeePerGas - base_fee)
+		# elif tx.gasPrice is not None and tx.type == '2':  
+	 #        	priority_fee = tx.maxPriorityFeePerGas
+		# else:
+	 #        	priority_fee = tx.gasPrice - base_fee
+		# 	# raise ValueError(f"Unsupported transaction type: {tx.type}")
+		if tx.type = '2':
+			priority_fee = 0
 		priority_fees.append(priority_fee)
 
 	for i in range(1, len(priority_fees)):
