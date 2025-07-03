7f91b53e5abd37747059da9817d5b41263d61b0f
Shijia003 <68273740+Shijia003@users.noreply.github.com>
2025-07-03 17:15:00 +0800

    Update salted_hashing.py

--- salted_hashing.py
+++ salted_hashing.py
@@ -4,12 +4,10 @@ import random
 
 def my_hash(m):
     #Generate random nonce
-    if not isinstance(m, str):
-        raise TypeError("Input must be a string")
     nonce = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
     #Generate hex digest
     combined = nonce + m
     hash_object = hashlib.sha256(combined.encode())
     h_hex = hash_object.hexdigest() 
-    assert len(hash_hex) == 64
+    assert len(h_hex) == 64
     return nonce, h_hex
